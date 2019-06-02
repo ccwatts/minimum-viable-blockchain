722da9919bc0f6aeaec578ed3cf012df7f79f4b2
ccwatts <cacwatts@gmail.com>
2019-06-02 13:58:58 -0700

    Tentative implementation of transaction verification and validation.

--- mvb.py
+++ mvb.py
@@ -1,31 +1,22 @@
 # aaaaa
 from node import *
 
-# there is probably a better way to do this.
-hash_bound = 0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
-coinbase_amount = 25
-
-NUM_NODES = 1
-
-def verify_hash(in_hash):
-    global hash_bound
-    numerical_value = int(in_hash, 16)
-    return numerical_value < hash_bound
-
+NUM_NODES = 2
 
 def initialize():
     for i in xrange(NUM_NODES):
         n = Node()
     cbtx = make_coinbase()
-
     for _, v in Node.nodes.items():
         v.accept_coinbase(cbtx)
+    return cbtx
+
 
 def make_coinbase():
     data = dict()
     chosen_node = random.choice(Node.nodes.keys())
-    data["input"] = list()
-    data["output"] = (chosen_node, coinbase_amount)
+    data["inputs"] = list()
+    data["outputs"] = [(chosen_node, Node.COINBASE_AMOUNT)]
     data["prev"] = None
     data["sigstrings"] = list()
     data["id"] = sha256(json.dumps(data))
@@ -33,21 +24,21 @@ def make_coinbase():
 
 
 if __name__ == "__main__":
-    '''
-    n = Node()
-    n2 = Node()
-    print n.pkh
-    print n2.pkh
-    print(make_coinbase())
-    '''
-    initialize()
-    for _, n in Node.nodes.items():
-        print n.chain
+    cbtx = initialize()
+    sender = Node.nodes[cbtx["outputs"][0][0]]
+    recipient = random.choice(Node.nodes.values())
+    
+    inputs = [(cbtx["id"], 0)]
+    outputs = [(sender.pkh, Node.COINBASE_AMOUNT)]
+
+    sent = sender.make_transaction(inputs, outputs, cbtx)
+    sender.mine(sent)
+    #print sender == recipient
+    #print sent
+    #sender.add_tx(sent)
+    print sender.validate(sent)
+    print sender.verify(sent)
     exit()
-    data = {"hello": "world"}
-    nonce = Node().proof_of_work(data)
-    print nonce
-    pass
 
 
 
--- node.py
+++ node.py
@@ -1,12 +1,22 @@
 import json
 import random
+from collections import OrderedDict
 from ecdsa import SigningKey
 from Crypto.Hash import SHA256
+
+
 
 def sha256(string):
     return SHA256.new(string).hexdigest()
 
+def verify_hash(in_hash):
+    numerical_value = int(in_hash, 16)
+    return numerical_value < Node.HASH_BOUND
+
 class Node:
+    # there is probably a better way to do this.
+    HASH_BOUND = 0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+    COINBASE_AMOUNT = 25
     nodes = dict()
     def __init__(self):
         # placeholders
@@ -14,20 +24,26 @@ class Node:
         self.pk = self.sk.get_verifying_key()
         self.pkh = sha256(self.pk.to_string())
         self.chain = None
+        self.tail = None
         Node.nodes[self.pkh] = self
 
     def accept_coinbase(self, coinbase_tx):
         self.chain = dict()
         self.chain[coinbase_tx["id"]] = coinbase_tx
+        self.tail = [coinbase_tx["id"]]
 
     def make_transaction(self, inputs, output, prev):  # , nonce, powork):
-        data = dict()
+        data = OrderedDict()
         # List of (ID hash, Offset) pairs
-        data["input"] = inputs
+        data["inputs"] = inputs
         # List of (ID hash, Amount) pairs
-        data["output"] = output
+        data["outputs"] = output
         # A singular ID to the previous "block"/transaction
-        data["prev"] = prev
+        # Wrap in an if statement so we can feed either the tx itself or its id
+        if type(prev) is dict:
+            data["prev"] = prev["id"]
+        else:
+            data["prev"] = prev
         # Proof of work parts
         # Is this done in advance or not?
         # data["nonce"] = None
@@ -36,9 +52,10 @@ class Node:
         # what we do ourselves.
         # make the signatures once the inputs/outputs are known
         sigstrings = list()
-        for pair in data["input"]:
+        for pair in data["inputs"]:
             content = self.get_sig_content(pair, output)
-            signer = Node.nodes[pair[0]]
+            output_source = self.get_output(pair)
+            signer = Node.nodes[output_source[0]]
             signature = signer.sign(content)
             sigstrings.append(signature)
 
@@ -50,16 +67,39 @@ class Node:
         return data
 
     def get_output(self, input_pair):
-        pkh = input_pair[0]
+        tx_id = input_pair[0]
         offset = input_pair[1]
-        return self.chain[pkh][offset]
+        return self.chain[tx_id]["outputs"][offset]
 
     def sign(self, m):
-        return self.sk.sign(m)
+        return self.sk.sign(m).encode("hex")
+
+    def verify_sigs(self, tx):
+        if len(tx["inputs"]) != len(tx["sigs"]):
+            return False
+        for i in range(len(tx["inputs"])):
+            pair = tx["inputs"][i]
+            content = self.get_sig_content(pair, tx["outputs"])
+            output_source = self.get_output(pair)
+            signer = Node.nodes[output_source[0]]
+            decoded_sig = tx["sigs"][i].decode("hex")
+            try:
+                signer.pk.verify(decoded_sig, content)
+            except AssertionError:
+                return False
+        return True
 
     def get_sig_content(self, input_pair, output):
         out_from_last = self.get_output(input_pair)  # self.chain[pkh][offset]
-        return json.dumps(input_pair) + json.dumps(out_from_last) + json.dumps(output)
+        return str(json.dumps(input_pair) + json.dumps(out_from_last) + json.dumps(output))
+
+    @staticmethod
+    def mine(tx):
+        nonce, hashed = Node.proof_of_work(tx)
+        tx["nonce"] = nonce
+        tx["pow"] = hashed
+        # need to do announcement stuff here
+        return tx
 
     @staticmethod
     def proof_of_work(tx):
@@ -72,39 +112,70 @@ class Node:
             nonce = random.SystemRandom().randint(1, 100000000000)
             total = serialized + str(nonce)
             hashed = sha256(total)
-        return nonce
+        return nonce, hashed
+
+    @staticmethod
+    def verify_pow(tx):
+        if tx["pow"] is not None and tx["nonce"] is not None:
+            without_pow = OrderedDict(tx)
+            without_pow.pop("pow")
+            without_pow.pop("nonce")
+            serialized = json.dumps(without_pow)
+            total = serialized + str(tx["nonce"])
+            rehash = sha256(total)
+            return rehash == tx["pow"]
+        else:
+            return False
 
     def has_double_spend(self, tx):
-        curr = self.chain[tx["prev"]]
-        while curr is not None:
-            # DO THINGS.
-            # if something in the output matches, ie double spent...
-            for pair in curr["output"]:
-                if pair in tx["output"]:
-                    return True
-            curr = self.chain[tx["prev"]]
+        for tail in self.tail:
+            try:
+                curr = self.chain[tail]
+                while curr is not None:
+                    # DO THINGS.
+                    # if something in the output matches, ie double spent...
+                    for pair in curr["inputs"]:
+                        if pair in tx["inputs"]:
+                            return True
+                    curr = self.chain[curr["prev"]]
+            except KeyError:
+                continue
         return False
 
     def io_matches(self, tx):
         amount_in = 0
-        for pk, offset in tx["input"]:
+        for pk, offset in tx["inputs"]:
             input_tx = self.chain[pk]
             # Transaction
             # -> Outputs [(ID, Amt)s]
             #    -> (ID, Amt)
             #       -> Amt
-            amount_in += input_tx["output"][offset][1]
+            amount_in += input_tx["outputs"][offset][1]
 
         amount_out = 0
-        for pk, amt in tx["output"]:
+        for pk, amt in tx["outputs"]:
             amount_out += amt
 
         return amount_in == amount_out
 
-    def verify(self, tx):
+    def validate(self, tx):
         # need to check:
         # does input match output?
-        # is the input verified?
         # did we double spend?
         # is the signature valid?
-        pass
+        return self.io_matches(tx) and self.verify_sigs(tx) and not self.has_double_spend(tx)
+
+    def verify(self, tx):
+        # is the input verified?
+        return self.validate(tx) and self.verify_pow(tx)
+
+    def add_tx(self, tx):
+        continues = False
+        for i in range(len(self.tail)):
+            if self.tail[i] == tx["prev"]:
+                self.tail[i] = tx["id"]
+                self.chain[tx["id"]] = tx
+                continues = True
+                break
+        if not continues:
+            raise Exception("Tried to add transaction that did not continue a current tail of the chain")
